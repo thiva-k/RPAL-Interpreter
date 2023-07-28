c5187546225d9c43ab8ea7a00794273bb03458ed
sthanikan2000 <thanikan.20@cse.mrt.ac.lk>
2023-07-28 22:27:38 +0530

    Added more possible argument passing

--- rpal20.java
+++ rpal20.java
@@ -2,9 +2,42 @@ import Engine.Evaluvator;
 
 public class rpal20 {
     
-    public static void main(String[] args) {   
-               
-        String fn = args[0];                                                    // get file name          
-        System.out.println(Evaluvator.evaluvate(fn));                                   // get and print the answer
+    public static void main(String[] args) {  
+        String fn;
+        boolean isPrintAST=false,isPrintST=false; 
+        if(args.length==0){
+            fn = "t1.txt"; 
+            isPrintAST = true;
+            isPrintST = true;
+            System.out.println(Evaluvator.evaluvate(fn,isPrintAST,isPrintST));                                   // get and print the answer
+
+        }
+        else if(args.length==3 && args[0].equalsIgnoreCase("-ast") && args[1].equalsIgnoreCase("-st")){
+            fn = args[2];
+            isPrintAST=true;
+            isPrintST=true;
+        }
+        else if(args.length==2){
+            fn=args[1];
+            if(args[0].equalsIgnoreCase("-ast")){
+                isPrintAST=true;
+            }
+            else if(args[0].equalsIgnoreCase("-st")){
+                isPrintST=true;
+            }
+            else{
+                System.out.println("Invalid Arguments Passing!");
+                return;
+            }
+        }
+        else if(args.length==1){
+            fn = args[0];
+        }
+        else{
+            System.out.println("Invalid Arguments Passing!");
+            return;
+        }
+
+                                                                 
     }
 }
