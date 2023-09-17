346b77f1392f2026720d3d2edfda73741b44e1c5
sthanikan2000 <thanikan.20@cse.mrt.ac.lk>
2023-09-17 16:20:42 +0530

    final commit

---
+++ Standardizer/AST.java
@@ -0,0 +1,33 @@
+package Standardizer;
+
+public class AST {
+    private Node root;
+    
+    public AST(Node root) {
+        this.setRoot(root);
+    }
+    
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+    
+    public Node getRoot() {
+        return this.root;
+    }
+    
+    public void standardize() {  
+        if (!this.root.isStandardized) {
+            this.root.standardize();
+        }
+    }
+    
+    private void preOrderTraverse(Node node,int i) {
+        for (int n = 0; n < i; n++) {System.out.print(".");}
+        System.out.println(node.getData());
+        node.children.forEach((child) -> preOrderTraverse(child, i+1));
+    }
+    
+    public void printAst() {
+        this.preOrderTraverse(this.getRoot(), 0);
+    }
+}
